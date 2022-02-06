db2d57576ae6332718538bb5b4c9981d60c5c88a
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2022-02-06 20:13:13 +0200

    Update firebase.ts

--- web/src/configs/firebase.ts
+++ web/src/configs/firebase.ts
@@ -8,8 +8,7 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
