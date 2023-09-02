6ef3c3b92136fed83f0c80408811dd91a9c06243
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-09-02 11:43:38 +0300

    Connect d

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,22 @@
 import app from "./app";
+import db from "@lib/db";
 import "dotenv/config";
 
-const port: number = Number(process.env.PORT);
+const start = async (): Promise<void> => {
+    console.log('Starting server');
 
-app.listen(port, (): void => {
-    console.log(`Server is running on port ${port}`);
-});
+    const port: number = Number(process.env.PORT);
+
+    try {
+        db.connect();
+        console.log('Connected to database');
+    } catch (error) {
+        console.error(error);
+    }
+
+    app.listen(port, (): void => {
+        console.log(`Server is running on port ${port}`);
+    });
+};
+
+start();
--- server/src/lib/types/env.d.ts
+++ server/src/lib/types/env.d.ts
@@ -1,5 +1,10 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         PORT: string;
+        DB_HOST: string;
+        DB_USER: string;
+        DB_PASSWORD: string;
+        DB_NAME: string;
+        DB_PORT: string;
     }
 }
